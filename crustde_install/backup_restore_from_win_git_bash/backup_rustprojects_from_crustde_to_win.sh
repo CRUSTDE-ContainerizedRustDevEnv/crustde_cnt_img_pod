9bd4e544a28d762852171f223befa23b83f30487
bestia.dev <info@bestia.dev>
2025-01-20 15:19:47 +0100

    sshadd crustde

--- crustde_install/backup_restore_from_win_git_bash/backup_rustprojects_from_crustde_to_win.sh
+++ crustde_install/backup_restore_from_win_git_bash/backup_rustprojects_from_crustde_to_win.sh
@@ -10,7 +10,7 @@ printf "\033[0;33m    Bash script to backup from CRUSTDE ~/rustprojects/  \033[0
 printf "\033[0;33m    to windows ~/rustprojects/crustde_rustprojects_backup/ \033[0m\n"
 printf "\033[0;33m    Use 'sshadd' to store the SSH passphrase. \033[0m\n"
 printf "\033[0;33m    Run this script in 'Windows git-bash':  \033[0m\n"
-printf "\033[0;32m sshadd \033[0m\n"
+printf "\033[0;32m sshadd crustde \033[0m\n"
 printf "\033[0;32m MSYS_NO_PATHCONV=1 sh //wsl.localhost/Debian/home/luciano/rustprojects/crustde_install/backup_restore_from_win_git_bash/backup_rustprojects_from_crustde_to_win.sh \033[0m\n"
 printf " \n"
 
--- crustde_install/backup_restore_from_win_git_bash/restore_rustprojects_from_win_to_crustde.sh
+++ crustde_install/backup_restore_from_win_git_bash/restore_rustprojects_from_win_to_crustde.sh
@@ -10,7 +10,7 @@ printf "\033[0;33m    Script to restore the folder '~/rustprojects' in CRUSTDE c
 printf "\033[0;33m    The backup file was transferer to Windows before upgrading the CRUSTDE container. \033[0m\n"
 printf "\033[0;33m    Use 'sshadd' to store the SSH passphrase. \033[0m\n"
 printf "\033[0;33m    Run this script in 'Windows git-bash':  \033[0m\n"
-printf "\033[0;32m sshadd \033[0m\n"
+printf "\033[0;32m sshadd crustde \033[0m\n"
 printf "\033[0;32m MSYS_NO_PATHCONV=1 sh //wsl.localhost/Debian/home/luciano/rustprojects/crustde_install/backup_restore_from_win_git_bash/restore_rustprojects_from_win_to_crustde.sh \033[0m\n"
 printf " \n"
 
@@ -26,10 +26,10 @@ ssh rustdevuser@crustde 'rm ~/crustde_rustprojects_backup.tar.gz'
 printf " \n"
 printf "\033[0;33m    Restore finished. \033[0m\n"
 printf "\033[0;33m    Run VSCode to connect to CRUSTDE rustprojects: \033[0m\n"
-printf "\033[0;32m sshadd \033[0m\n"
+printf "\033[0;32m sshadd crustde \033[0m\n"
 printf "\033[0;32m MSYS_NO_PATHCONV=1 code --remote ssh-remote+crustde /home/rustdevuser/rustprojects  \033[0m\n"
 
 printf "\033[0;33m    In VSCode terminal pull all from remote repository: \033[0m\n"
-printf "\033[0;32m sshadd \033[0m\n"
+printf "\033[0;32m sshadd crustde \033[0m\n"
 printf "\033[0;32m sh pull_all.sh \033[0m\n"
 printf " \n"
