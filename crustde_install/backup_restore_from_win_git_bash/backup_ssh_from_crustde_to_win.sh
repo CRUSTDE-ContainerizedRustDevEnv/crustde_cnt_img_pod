f58adabfe98fb9c30dbd88412103bc2e010793bb
bestia.dev <info@bestia.dev>
2025-04-06 18:22:28 +0200

    backup

--- crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_crustde_to_win.sh
+++ crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_crustde_to_win.sh
@@ -3,7 +3,7 @@
 # backup_ssh_from_crustde_to_win.sh
 # repository: https://github.com/CRUSTDE-ContainerizedRustDevEnv/crustde_cnt_img_pod
 # run in Windows git-bash:
-# MSYS_NO_PATHCONV=1 sh //wsl.localhost/Debian/home/luciano/rustprojects/crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_crustde_to_win.sh
+# sh backup_ssh_from_crustde_to_win.sh
 
 printf " \n"
 printf "\033[0;33m    Bash script to backup from CRUSTDE ~/.ssh/  \033[0m\n"
@@ -11,13 +11,16 @@ printf "\033[0;33m    to windows ~/.ssh/crustde_ssh_backup/ \033[0m\n"
 printf "\033[0;33m    Use 'sshadd' to store the SSH passphrase. \033[0m\n"
 printf "\033[0;33m    Run this script in 'Windows git-bash':  \033[0m\n"
 printf "\033[0;32m sshadd crustde \033[0m\n"
-printf "\033[0;32m MSYS_NO_PATHCONV=1 sh //wsl.localhost/Debian/home/luciano/rustprojects/crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_crustde_to_win.sh \033[0m\n"
+printf "\033[0;32m sh backup_ssh_from_crustde_to_win.sh \033[0m\n"
 printf " \n"
 
-printf "\033[0;33m    ssh rustdevuser@crustde 'tar -czvf crustde_ssh_backup.tar.gz /home/rustdevuser/.ssh/'  \033[0m\n"
-ssh rustdevuser@crustde 'tar -czvf crustde_ssh_backup.tar.gz /home/rustdevuser/.ssh/'
 printf "\033[0;33m    mkdir -p ~/.ssh/crustde_ssh_backup \033[0m\n"
 mkdir -p ~/.ssh/crustde_ssh_backup
+cd ~/.ssh/crustde_ssh_backup
+
+printf "\033[0;33m    ssh rustdevuser@crustde 'tar -C /home/rustdevuser/.ssh -czvf crustde_ssh_backup.tar.gz .'  \033[0m\n"
+ssh rustdevuser@crustde 'tar -C /home/rustdevuser/.ssh -czvf crustde_ssh_backup.tar.gz .'
+
 printf "\033[0;33m    MSYS_NO_PATHCONV=1 scp rustdevuser@crustde:~/crustde_ssh_backup.tar.gz ~/.ssh/crustde_ssh_backup/crustde_ssh_backup.tar.gz \033[0m\n"
 MSYS_NO_PATHCONV=1 scp rustdevuser@crustde:~/crustde_ssh_backup.tar.gz ~/.ssh/crustde_ssh_backup/crustde_ssh_backup.tar.gz
 printf "\033[0;33m    ssh rustdevuser@crustde 'rm crustde_ssh_backup.tar.gz' \033[0m\n"
--- crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_wsl_to_win.sh
+++ crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_wsl_to_win.sh
@@ -3,20 +3,24 @@
 # backup_ssh_from_wsl_to_win.sh
 # repository: https://github.com/CRUSTDE-ContainerizedRustDevEnv/crustde_cnt_img_pod
 # run in Windows git-bash:
-# MSYS_NO_PATHCONV=1 sh //wsl.localhost/Debian/home/luciano/rustprojects/crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_wsl_to_win.sh
+# backup_ssh_from_wsl_to_win.sh
 
 printf " \n"
 printf "\033[0;33m    Bash script to backup personal data from WSL ~/.ssh/  \033[0m\n"
 printf "\033[0;33m    to windows ~/.ssh/wsl_ssh_backup/ \033[0m\n"
 printf "\033[0;33m    This backup of personal data can then survive the reset of the WSL2 virtual machine. \033[0m\n"
 printf "\033[0;33m    Run this script in 'Windows git-bash':  \033[0m\n"
-printf "\033[0;32m MSYS_NO_PATHCONV=1 sh //wsl.localhost/Debian/home/luciano/rustprojects/crustde_install/backup_restore_from_win_git_bash/backup_ssh_from_wsl_to_win.sh \033[0m\n"
+printf "\033[0;32m backup_ssh_from_wsl_to_win.sh \033[0m\n"
 printf " \n"
 
 printf "\033[0;33m    mkdir -p ~/.ssh/wsl_ssh_backup \033[0m\n"
 mkdir -p ~/.ssh/wsl_ssh_backup
+cd ~/.ssh/wsl_ssh_backup
 
-printf "\033[0;33m    tar -czvf wsl_ssh_backup.tar.gz /home/rustdevuser/.ssh/'  \033[0m\n"
-tar -czvf ~/.ssh/wsl_ssh_backup/wsl_ssh_backup.tar.gz //wsl.localhost/Debian/home/luciano/.ssh
+# put current date as yyyy-mm-dd in $date
+date=$(date '+%Y-%m-%d')
+
+printf "\033[0;33m    tar -C //wsl.localhost/Debian/home/luciano/.ssh -czvf "${date}_wsl_ssh_backup.tar.gz" . \033[0m\n"
+tar -C //wsl.localhost/Debian/home/luciano/.ssh -czvf "${date}_wsl_ssh_backup.tar.gz" .
 
 printf " \n"
