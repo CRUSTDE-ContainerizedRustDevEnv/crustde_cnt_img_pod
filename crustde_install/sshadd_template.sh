0a944eea78993b6d9bf4a57d2009e398206f4500
bestia.dev <info@bestia.dev>
2025-08-12 21:35:59 +0200

    sshadd

--- crustde_install/sshadd_template.sh
+++ crustde_install/sshadd_template.sh
@@ -1,48 +1,49 @@
 #!/bin/sh
 
 # ~/.ssh/sshadd.sh for container
-# Inside this template, replace the words your_key_for_github_ssh_1, your_key_for_webserver_ssh_1 and your_key_for_github_api_token_ssh_1
-# with your identity file names for the ssh private key.
+# List all private keys in ~/.ssh and offer ssh-add for them.
+# The command argument can filter only for keys that contain the string.
 
-printf " \n"
-printf "  \033[33m   Add often used SSH identity private keys to ssh-agent \033[0m\n"
-printf "  \033[33m   If you add an argument: 'github' or 'your_webserver' or 'github_api_token' you will add only that credential. \033[0m\n"
+printf "\n"
+printf "\033[33m  Add often used SSH identity private keys to ssh-agent \033[0m\n"
 
-printf " \n"
-printf "  \033[33m   The ssh-agent should be started already on login inside the ~/.bashrc script. \033[0m\n"
-printf "  \033[33m <https://github.com/CRUSTDE-ContainerizedRustDevEnv/crustde_cnt_img_pod/blob/main/ssh_easy.md> \033[0m\n"
-printf "  \033[33m   It is recommended to use the ~/.ssh/config file to assign explicitly one ssh key to one ssh server. \033[0m\n"
-printf "  \033[33m   If not, ssh-agent will send all the keys to the server and the server could refute the connection because of too many bad keys. \033[0m\n"
+printf "\n"
+printf "\033[33m  The ssh-agent should be started already on login inside the ~/.bashrc script. \033[0m\n"
+printf "\033[33m  <https://github.com/CRUSTDE-ContainerizedRustDevEnv/crustde_cnt_img_pod/blob/main/ssh_easy.md> \033[0m\n"
+printf "\033[33m  It is recommended to use the ~/.ssh/config file to assign explicitly one ssh key to one ssh server. \033[0m\n"
+printf "\033[33m  If not, ssh-agent will send all the keys to the server and the server could refute the connection because of too many bad keys. \033[0m\n"
 
-printf " \n"
-# check the content of ~/.ssh/config if it contains all the ssh keys
-cat ~/.ssh/config | grep -q "~/.ssh/your_key_for_github_ssh_1" || printf "  \033[31m   The ~/.ssh/config does not contain the identity ~/.ssh/your_key_for_github_ssh_1. \033[0m\n"
-cat ~/.ssh/config | grep -q "~/.ssh/your_key_for_webserver_ssh_1" || printf "  \033[31m   The ~/.ssh/config does not contain the identity ~/.ssh/your_key_for_webserver_ssh_1. \033[0m\n"
-cat ~/.ssh/config | grep -q "~/.ssh/your_key_for_github_api_token_ssh_1" || printf "  \033[31m   The ~/.ssh/config does not contain the identity ~/.ssh/your_key_for_github_api_token_ssh_1. \033[0m\n"
+# List the private key files inside ~/.ssh
+printf "\n"
+printf "\033[33m  List identities inside ssh-agent:  \033[0m\n"
+ssh-add -l
+printf "\n"
+printf "\033[33m  Identities you can add to ssh-agent: \033[0m\n"
+printf "\033[33m  Enter passphrase or press enter to skip. \033[0m\n"
+printf "\n"
 
+for entry in ~/.ssh/*.pub 
+do
+  # remove suffix .pub
+  full_file_name=${entry%.pub}
+  # echo "$full_file_name"
+  # remove prefix until the last slash (5th segment)
+  file_name=$(echo $full_file_name |  cut -d '/' -f 5 )
+  # echo "$file_name"
+  if  [ $# -eq 0 ] || (echo "$file_name" | grep -q "$1") ; then
+    ssh-add -l | grep -q `ssh-keygen -lf "$full_file_name" | awk '{print $2}'` || (printf "  \033[33m ssh-add -t 1h $full_file_name \033[0m\n" & ssh-add -t 1h $full_file_name)
+  else
+    ssh-add -l | grep -q `ssh-keygen -lf "$full_file_name" | awk '{print $2}'` || printf "Skip by argument: $file_name \n"
+  fi
+done
 
-if [ $# -eq 0 ] || [ $1 = "github" ]; then
-    # add if key not yet exist in ssh-agent for git@github.com
-    ssh-add -l | grep -q `ssh-keygen -lf ~/.ssh/your_key_for_github_ssh_1 | awk '{print $2}'` || (printf "  \033[33m github \033[0m\n" & ssh-add -t 1h ~/.ssh/your_key_for_github_ssh_1)
-fi
-
-if [ $# -eq 0 ] || [ $1 = "your_webserver" ]; then
-    # add if key not yet exist in ssh-agent for your_username@your_webserver
-    ssh-add -l | grep -q `ssh-keygen -lf ~/.ssh/your_key_for_webserver_ssh_1 | awk '{print $2}'` || (printf "  \033[33m your_webserver \033[0m\n" & ssh-add -t 1h ~/.ssh/your_key_for_webserver_ssh_1)
-fi
-
-if [ $# -eq 0 ] || [ $1 = "github_api_token" ]; then
-    # add if key not yet exist in ssh-agent for your_key_for_github_api_token_ssh_1
-    ssh-add -l | grep -q `ssh-keygen -lf ~/.ssh/your_key_for_github_api_token_ssh_1 | awk '{print $2}'` || (printf "  \033[33m github_api_token \033[0m\n" & ssh-add -t 1h ~/.ssh/your_key_for_github_api_token_ssh_1)
-fi
-
-printf " \n"
-printf "  \033[33m   The keys are set to expire in 1 hour. \033[0m\n"
-printf "  \033[33m   For security, when you are finished using the keys, remove them from the ssh-agent with: \033[0m\n"
-printf "\033[32m ssh-add -D \033[0m\n"
-printf " \n" 
-printf "   \033[33m   List public fingerprints inside ssh-agent: \033[0m\n"
-printf "\033[32m ssh-add -l \033[0m\n"
+printf "\n"
+printf "\033[33m  The keys are set to expire in 1 hour. \033[0m\n"
+printf "\033[33m  For security, when you are finished using the keys, remove them from the ssh-agent with: \033[0m\n"
+printf "\033[32mssh-add -D \033[0m\n"
+printf "\n" 
+printf "\033[33m  List public fingerprints inside ssh-agent: \033[0m\n"
+printf "\033[32mssh-add -l \033[0m\n"
 ssh-add -l
 
-printf " \n"
+printf "\n"
